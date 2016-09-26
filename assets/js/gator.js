5faaf004b17b6119dc2ff6371c1c68dc30d85ec4
Steven Freeman <sfreeman422@gmail.com>
2016-09-26 10:44:15 -0400

    Implemented Tariqs code to parse the JSON from a subreddit and post it to the proper div on our front end

--- assets/js/gator.js
+++ assets/js/gator.js
@@ -7,3 +7,17 @@
     messagingSenderId: "866976750728"
   };
   firebase.initializeApp(config);
+
+  //Grabs the JSON and parses it for the default subreddits. This code should be designed to only execute when the user isnt logged in.
+  $.getJSON(
+        "http://www.reddit.com/r/news+worldnews.json?jsonp=?",
+        function foo(data)
+        { console.log(data)
+          $.each(
+            data.data.children.slice(0, 25),
+            function (i, post) {
+              $("#reddit").append( "<li class = 'collection-item avatar><img src = '"+post.data.url+"' alt ='' class = 'circle><span class='title'>" + post.data.title+"</span><br><a href = '"+post.data.url+"'>View on Reddit!</a></li>");
+            }
+          )
+        }
+      );
