805ffa4110c79392a8b6d09eda47313c6e22655b
mikedeleamon <ma3971a@student.american.edu>
2016-09-26 21:07:55 -0400

    active NYT Searchbar

--- assets/js/gator.js
+++ assets/js/gator.js
@@ -1,4 +1,6 @@
-// Initialize Firebase
+console.log("page loaded");
+
+Initialize Firebase
   var config = {
     apiKey: "AIzaSyCA22_-RWAMuD6vTue3XwjmBlp-0kwCniw",
     authDomain: "gatordb-62e35.firebaseapp.com",
@@ -7,3 +9,70 @@
     messagingSenderId: "866976750728"
   };
   firebase.initializeApp(config);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var authKey = "9d4a8986921972b65754ea0809d47c84:12:74623931";
+
+var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey;
+
+var question= '';
+
+var fullQuery = '';
+
+var queryURL='';
+
+$("#nytSearch").on('keyup',function(e){ 
+    e.preventDefault();
+    var code = e.which;
+    if(code==13)
+    {
+      console.log("yay!");
+        question =$("#nytSearch").val().trim();
+        var rSpace = question.split(' ').join('+');
+	fullquery= queryURLBase+'&q='+ rSpace;
+	console.log(fullquery);
+  queryURL=fullQuery
+    } 
+
+
+
+	
+
+
+  
+  	$.ajax({
+            url: queryURL,
+            method: 'GET'
+        })
+
+  	.done(function(response) {
+		  	console.log(response)
+		  })
+		  	return false;
+		  });
