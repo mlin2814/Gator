8796019c666f12f181222614398056654fdf266f
Steven Freeman <sfreeman422@gmail.com>
2016-09-29 00:12:18 -0400

    Removed more unnecessary files

--- assets/js/firebaseinit.js
+++
@@ -1,9 +0,0 @@
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCA22_-RWAMuD6vTue3XwjmBlp-0kwCniw",
-    authDomain: "gatordb-62e35.firebaseapp.com",
-    databaseURL: "https://gatordb-62e35.firebaseio.com",
-    storageBucket: "gatordb-62e35.appspot.com",
-    messagingSenderId: "866976750728"
-  };
-  firebase.initializeApp(config);
